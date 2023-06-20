2926188a283cd2ca7d0de8495ae384f0034b0e9a
Andy Zhang <67079259+azhang777@users.noreply.github.com>
2023-06-20 11:50:16 -0400

    update
    yesterday's commit, looking for a way to customize typography and implement it using the theme context

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
+import { useContext } from "react";
 import { BrowserRouter } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import HomePage from "./scenes/landingPage";
+import { themeSettings } from "./theme";
 
 function App() {
+  const theme = themeSettings();
   return (
     <div className="App">
       <BrowserRouter>
---
+++ client/src/theme.js
@@ -0,0 +1,33 @@
+// mui theme settings
+export const themeSettings = () => {
+  return {
+    typography: {
+      fontFamily: ["Lato", "sans-serif"],
+      fontSize: 12,
+      h1: {
+        fontFamily: ["Lato", "sans-serif"],
+        fontSize: 40,
+      },
+      h2: {
+        fontFamily: ["Lato", "sans-serif"],
+        fontSize: 32,
+      },
+      h3: {
+        fontFamily: ["Lato", "sans-serif"],
+        fontSize: 24,
+      },
+      h4: {
+        fontFamily: ["Lato", "sans-serif"],
+        fontSize: 20,
+      },
+      h5: {
+        fontFamily: ["Lato", "sans-serif"],
+        fontSize: 16,
+      },
+      h6: {
+        fontFamily: ["Lato", "sans-serif"],
+        fontSize: 14,
+      },
+    },
+  };
+};
